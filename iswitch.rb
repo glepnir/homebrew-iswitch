744a8b5be9a37ec39244db9cd35f96c249c7578d
glepnir <glephunter@gmail.com>
2025-03-16 19:07:49 +0800

    update

--- iswitch.rb
+++ iswitch.rb
@@ -8,8 +8,18 @@ class Iswitch < Formula
     bin.install "iswitch"
   end
 
+  service do
+    run [opt_bin/"iswitch"]
+    keep_alive true
+    log_path var/"log/iswitch.log"
+    error_log_path var/"log/iswitch.log"
+    working_dir HOMEBREW_PREFIX
+  end
+
   test do
-    system "#{bin}/iswitch", "-h"
+    # Test help output
+    assert_match "iSwitch", shell_output("#{bin}/iswitch -h", 0)
+    system "#{bin}/iswitch", "-p" or true
   end
 
   livecheck do
