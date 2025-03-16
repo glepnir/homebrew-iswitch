bdfc2468421a7ae8c2b960617ec62b5a62ca8362
glepnir <glephunter@gmail.com>
2025-03-16 19:16:01 +0800

    update

--- iswitch.rb
+++ iswitch.rb
@@ -1,8 +1,15 @@
 class Iswitch < Formula
   desc "A tool to switch input sources on macOS"
   homepage "https://github.com/glepnir/iswitch"
-  url "https://github.com/glepnir/iswitch/releases/latest/download/iswitch-macos.tar.gz"
+  url "https://github.com/glepnir/iswitch/releases/latest/download/iswitch.tar.gz"
+  version :head  # Special version designation that tells Homebrew this is always the latest
+  sha256 :no_check  # Skip checksum verification since we're using "latest"
   license "MIT"
+
+  # Add head to allow installation from GitHub source
+  head do
+    url "https://github.com/glepnir/iswitch.git", branch: "main"
+  end
 
   def install
     bin.install "iswitch"
@@ -17,11 +24,16 @@ class Iswitch < Formula
   end
 
   test do
-    system "#{bin}/iswitch", "-h"
+    # Test help output
+    assert_match "iSwitch", shell_output("#{bin}/iswitch -h", 0)
+
+    # Test listing input sources (this might fail if run in CI without a GUI)
+    # so we make it optional with the true || part
+    system "#{bin}/iswitch", "-p" or true
   end
 
+  # Disable livecheck since we're always using the latest version
   livecheck do
-    url :stable
-    strategy :github_latest
+    skip "Uses latest version"
   end
 end
